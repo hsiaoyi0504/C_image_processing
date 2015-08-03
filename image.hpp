ea0bfd3aab42dc29dc091a03a161ba4b6a3c62de
hsiaoyi0504 <hsiaoyi0504@gmail.com>
2015-08-03 14:42:22 +0800

    add getter to Image class

--- image.hpp
+++ image.hpp
@@ -1,16 +1,16 @@
 #include <stdio.h>
 #include <math.h>
 
-class image
+class Image
 {
 	private:
 		int width,height,channels;
 		unsigned char* data;
 	public:
-		image(int w,int h,int c){
+		Image(int w,int h,int c){
 			data=new unsigned char[w*h*c];
 		}
-		~image() {
+		~Image() {
 			delete [] data;
 		}
 		bool readRaw(char* filename){
@@ -32,5 +32,14 @@ class image
 		}
 		unsigned char getPixel(int w,int h,int c){
 			return data[(h*width+w)*channels+c];
+		}
+		int getWidth(){
+			return width;
+		}
+		int getHeight(){
+			return height;
+		}
+		int getChannels(){
+			return channels;
 		}
 }
