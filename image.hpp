0695c844d4d6a18a59aa995ce49d786f2caec5b6
hsiaoyi0504 <hsiaoyi0504@gmail.com>
2015-08-03 16:05:55 +0800

    changed to more fine coding style,add PSNR function

--- image.hpp
+++ image.hpp
@@ -8,75 +8,21 @@ class Image
 		unsigned char* data;
 		string colorspace;
 	public:
-		Image(){
-			data=NULL;
-		}
-		Image(int w,int h,int c){
-			data=new unsigned char[w*h*c];
-		}
-		~Image() {
-			delete [] data;
-		}
-		bool readRaw(char* filename){
-			FILE * readPtr=fopen(filename,"rb");
-			if(readPtr!=NULL){
-				for(int i=0;i<height;i++){
-					for(int j=0;j<width;j++){
-						for(int k=0:k<channels;k++){
-							data[(i*width+j)*channels+k]=fgetc(readPtr);
-						}
-					}
-				}
-				fclose(readPtr);
-				colorspace="YUV";
-				return true;
-			}
-			else{
-				return false;
-			}
-		}
-		unsigned char getPixel(int w,int h,int c){
-			return data[(h*width+w)*channels+c];
-		}
-		int getWidth(){
-			return width;
-		}
-		int getHeight(){
-			return height;
-		}
-		int getChannels(){
-			return channels;
-		}
-		unsigned char* getData(){
-			return data;
-		}
-		string getColorspace(){
-			return colorspace;
-		}
-		void setData(unsigned char* d){
-			data=d;
-		}
-		void setWidth(int w){
-			width=w;
-		}
-		void setHeight(int h){
-			height=h;
-		}
-		void setChannels(int c){
-			channels=c;
-		}
-		void setColorSpace(string color){
-			colorspace=color;
-		}
-		void setImageInfo(int w,int h,int c){
-			width=w;
-			height=h;
-			channels=c;
-		}
-		void setImageInfo(int w,int h,int c,string color){
-			width=w;
-			height=h;
-			channels=c;
-			colorspace=color;
-		}
+		Image()
+		Image(int w,int h,int c)
+		~Image()
+		bool readRaw(char* filename);
+		unsigned char getPixel(int w,int h,int c);
+		int getWidth();
+		int getHeight();
+		int getChannels();
+		unsigned char* getData();
+		string getColorspace();
+		void setData(unsigned char* d);
+		void setWidth(int w);
+		void setHeight(int h);
+		void setChannels(int c);
+		void setColorSpace(string color);
+		void setImageInfo(int w,int h,int c);
+		void setImageInfo(int w,int h,int c,string color);
 };
